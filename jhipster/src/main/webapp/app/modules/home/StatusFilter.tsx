00cd269356e2a4b23cf7252d3959edf74f8d586e
costeau <aurelien.coste@etu.univ-grenoble-alpes.fr>
2023-12-19 17:36:59 +0100

    CSS changes

--- jhipster/src/main/webapp/app/modules/home/StatusFilter.tsx
+++ jhipster/src/main/webapp/app/modules/home/StatusFilter.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const StatusFilter = ({ handleStatusFilterChange }) => {
+const StatusFilter = ({ handleStatusFilterChange}) => {
 
   const statusOptions = [
     { value: 'surveillance prioritaire', label: 'Surveillance prioritaire' },
@@ -8,17 +8,39 @@ const StatusFilter = ({ handleStatusFilterChange }) => {
     { value: 'normal', label: 'Normal' },
   ];
 
+  const [checkboxStates, setCheckboxStates] = useState(
+    Object.fromEntries(statusOptions.map((option) => [option.value, true]))
+  );
+
+  const handleCheckboxChange = (e, optionValue) => {
+    // Compte le nombre de cases cochées
+    const checkedCheckboxes = Object.values(checkboxStates).reduce((count, isChecked) => (isChecked ? count + 1 : count), 0);
+
+    // Si une seule case est cochée et qu'on la décocher, pas d'action
+    if (checkedCheckboxes === 1 && checkboxStates[optionValue]) {
+      return;
+    }
+
+    // Sinon on change les états des checkbox et on appel le tri des patient
+    setCheckboxStates((prevStates) => ({
+      ...prevStates,
+      [optionValue]: !prevStates[optionValue],
+    }));
+    handleStatusFilterChange(optionValue, e.target.checked)
+  };
+
   return (
     <div className="status-filter-container">
           {statusOptions.map((option) => (
             <div key={option.value}>
               <input
+                className='input_statut'
                 type="checkbox"
                 id={`status-${option.value}`}
                 name={`status-${option.value}`}
                 value={option.value}
-                // ajouter le fait que les cases soient cochées de base puis qu'elles puissent être décochées checked={}
-                onChange={(e) => handleStatusFilterChange(option.value, e.target.checked)}
+                checked={checkboxStates[option.value]}
+                onChange={(e) => handleCheckboxChange(e, option.value)}
               />
               <label htmlFor={`status-${option.value}`} className="checkbox-label">
                 {option.label}
